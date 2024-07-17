7b702462a7fb42cf1d6080284b5c5c38f6a5a1c4
novikoff <novikoff04@gmail.com>
2024-07-17 11:51:12 -0700

    added useState, input check

--- src/components/Notes/Notes.jsx
+++ src/components/Notes/Notes.jsx
@@ -1,13 +1,33 @@
-import React from 'react'
+import cn from 'classnames'
+import React, { useState } from 'react'
+import { FaCheck } from 'react-icons/fa6'
 import classes from './Notes.module.css'
 
 const Notes = props => {
+	const [checked, setChecked] = useState(false)
+
+	const toggleCheck = () => {
+		setChecked(prevChecked => !prevChecked)
+	}
+
 	return (
 		<div className={classes.Notes}>
 			<div className={classes.NoteWrapper}>
 				<div className={classes.Note}>
-					<div className={classes.Input} />
-					<span>The first note</span>
+					<div
+						className={cn(classes.Input, {
+							[classes.checkedBackground]: checked
+						})}
+						onClick={() => toggleCheck()}
+					>
+						<div className={classes.CheckIconContainer}>
+							{checked && <FaCheck className={classes.checkIcon} />}
+						</div>
+					</div>
+
+					<span className={cn({ [classes.CheckedText]: checked })}>
+						The first note
+					</span>
 				</div>
 				<div className={classes.tools}>
 					<button>Edit</button>
